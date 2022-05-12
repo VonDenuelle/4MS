335919d4f27532ab307f9771a628d87144630f72
Justin Edrique Reyes <55437786+reyesjustinedrique@users.noreply.github.com>
2022-05-12 14:51:46 +0800

    about page...lol

---
+++ about.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <!-- Meta Tags -->
+      <?php include_once 'php/global/head.php'; ?>
+
+      <link rel="stylesheet" href="assets/css/navbar.css">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+      <link rel="stylesheet" href="css/about.css">
+
+      <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+      integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+      <title>About Page</title>
+   </head>
+   <body>
+      <?php include_once 'php/global/navbar.php'; ?>
+
+      <div class="container">
+         <div class="row mx-auto mt-3">
+            <div class="col-3 mx-auto mt-2">
+               <img class="mlogo ms-5" src="images/4M's-Logo.png">
+            </div>
+
+            <div class="col-9 mt-4">
+               <h1>About Us</h1>
+               <p class="par">"We give you the best flowers available with each picked with care 
+                  and beatifully arranged, to fulfill your heart's desire and elegance."
+               </p>
+            </div>
+         </div>
+
+         <div class="row mt-5 mx-auto">
+            <div class="col-6 mt-2">
+               <h1 class="title">Location</h1>
+               <p class="par2">#31 18th St., East Bajac Bajac 2200 Olongapo City,<br>Philippines
+               </p>
+               <h3 class="title">Open Hours:</h3>
+               <p class="par2">8AM - 6PM</p>
+            </div>
+            
+            <div class="col-5 mt-4">
+               <img class="loc" src="images/location.png">
+            </div>
+         </div>
+      </div>
+
+      
+      <?php include_once 'php/global/footer.php'; ?>
+      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+   </body>
+</html>
